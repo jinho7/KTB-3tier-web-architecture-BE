aa4af84cc131a7deb1bcbe9304c95fd6ade157bf
jinho7 <69239293+jinho7@users.noreply.github.com>
2025-04-01 14:56:32 +0900

    Remove tracked files and apply .gitignore

--- src/main/java/com/example/KTB_3tier_web_architecture_BE/repository/CounterRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.KTB_3tier_web_architecture_BE.repository;
-
-import com.example.KTB_3tier_web_architecture_BE.entity.Counter;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CounterRepository extends JpaRepository<Counter, Long> {
-}
